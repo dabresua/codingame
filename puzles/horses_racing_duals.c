35dd56fae9674a7078dfd085b636a6c4106e869b
Daniel Breton <d.breton.suarez@gmail.com>
2020-04-22 20:03:20 +0200

    First algorithm not optimized

--- puzles/horses_racing_duals.c
+++ puzles/horses_racing_duals.c
@@ -8,19 +8,35 @@
  * the standard input according to the problem statement.
  **/
 
+#define MAX_N  (100000)
+#define MAX_PI (10000000)
+
 int main()
 {
 	int N;
 	scanf("%d", &N);
+	int pi[N];
+	int min_diff = MAX_PI;
+	fprintf(stderr, "N %d\n", N);
 	for (int i = 0; i < N; i++) {
-		int pi;
-		scanf("%d", &pi);
+		scanf("%d", &pi[i]);
+		fprintf(stderr, "pi %d\n", pi[i]);
+		int min = MAX_PI;
+		for(int j = 0; j < i; j++) {
+			int mij = (pi[j] > pi[i])?(pi[j] - pi[i]):(pi[i] - pi[j]);
+			if(mij < min) {
+				min = mij;
+			}
+		}
+		if(min < min_diff) {
+			min_diff = min;
+		}
 	}
 
 	// Write an answer using printf(). DON'T FORGET THE TRAILING \n
 	// To debug: fprintf(stderr, "Debug messages...\n");
 
-	printf("answer\n");
+	printf("%d\n", min_diff);
 
 	return 0;
 }
